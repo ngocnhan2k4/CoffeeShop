87bf7f4c5a93b3b8cf06bbb904bfdaa04ee94891
NHÂN TRẦN NGỌC <tranngocnhannt2004@gmail.com>
2024-10-20 17:04:00 +0700

    #1 - design dashboard page

---
+++ CoffeeShop/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    /// <summary>
+    /// This class represents a category of products
+    /// </summary>
+    public class Category : INotifyPropertyChanged
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
