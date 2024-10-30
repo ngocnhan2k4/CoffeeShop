ae828f38468308c3c0e9e83683bd7cbafb29e741
“nhankhtn” <nguyenanhnguyen3006@gmail.com>
2024-10-30 09:05:55 +0700

    change name file

--- CoffeeShop/Service/DataAccess/IDAO.cs
+++ CoffeeShop/Service/DataAccess/IDAO.cs
@@ -8,7 +8,20 @@ namespace CoffeeShop.Service.DataAccess
 {
     public interface IDao
     {
-        List<Drink> GetAllDrinks();
+        public enum SortType
+        {
+            Ascending,
+            Descending
+        }
+        List<Drink> GetDrinks();
+        public Tuple<List<Drink>, int> GetDrinks(
+            int page, int rowsPerPage,
+            string keyword, int categoryID,
+            Dictionary<string, SortType> sortOptions
+        );
         List<Category> GetCategories();
+        List<DeliveryInvoice> GetDeliveryInvoices();
+        List<Invoice> GetInvoices();
+        List<DetailInvoice> GetDetailInvoices();
     }
 }
