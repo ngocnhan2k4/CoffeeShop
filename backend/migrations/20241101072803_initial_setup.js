6ba6e583887792f3d5c8ec67423d842b754f5f1f
Minh Nhat <ndminhnhat1234@gmail.com>
2024-11-29 19:02:46 +0700

    fix trigger

--- backend/migrations/20241101072803_initial_setup.js
+++ backend/migrations/20241101072803_initial_setup.js
@@ -47,35 +47,6 @@ exports.up = async function (knex) {
         );
     `);
 
-    await knex.raw(`
-        CREATE TRIGGER trg_decrease_stock
-        ON invoice_detail
-        AFTER INSERT
-        AS
-        BEGIN
-            UPDATE drink
-            SET stock = stock - inserted.quantity
-            FROM drink
-            INNER JOIN inserted ON drink.id = inserted.drink_id
-        END;
-    `);
-    await knex.raw(`
-        CREATE TRIGGER trg_increase_stock_on_cancel
-        ON invoice
-        AFTER UPDATE
-        AS
-        BEGIN
-            IF UPDATE(status)
-            BEGIN
-                UPDATE drink
-                SET stock = stock + invoice_detail.quantity
-                FROM drink
-                INNER JOIN invoice_detail ON drink.id = invoice_detail.drink_id
-                INNER JOIN inserted ON invoice_detail.invoice_id = inserted.id
-                WHERE inserted.status = 'Cancel';
-            END
-        END;
-    `);
 };
 
 exports.down = async function (knex) {
